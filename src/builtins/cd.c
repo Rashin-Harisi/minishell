0e14397f259e8fe326327f36cfd7be1bec9aa9a8
Rashin-Harisi <rashin.harisi@gmail.com>
2026-07-14 11:51:01 +0200

    save current and old directories based on different scenario and the pwd that is in env list

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -4,8 +4,10 @@ int cd_env_func(char **args, t_shell *shell)
 {
     char *path;
     char *oldpwd_env;
+    char *pwd_env;
     char old_pwd[PATH_MAX];
     char curr_pwd[PATH_MAX];
+    int old_pwd_valid;
 
     if (args[1] && args[2])
     {
@@ -13,8 +15,14 @@ int cd_env_func(char **args, t_shell *shell)
         shell->exit_status = 1;
         return (1);
     }
+    old_pwd_valid = 1;
     if (!getcwd(old_pwd, sizeof(old_pwd)))
-        return (perror("getcwd"), shell->exit_status = 1, 1);
+    {
+        old_pwd_valid = 0;
+        pwd_env = get_env_value(shell->env, "PWD");
+        if (pwd_env)
+            ft_strlcpy(old_pwd, pwd_env, sizeof(old_pwd));
+    }
     if (!args[1])
     {
         path = get_env_value(shell->env, "HOME");
@@ -25,7 +33,7 @@ int cd_env_func(char **args, t_shell *shell)
             return (1);
         }
     }
-    else if (ft_strncmp(args[1],"-",2) == 0)
+    else if (ft_strncmp(args[1], "-", 2) == 0)
     {
         oldpwd_env = get_env_value(shell->env, "OLDPWD");
         if (!oldpwd_env)
@@ -35,8 +43,6 @@ int cd_env_func(char **args, t_shell *shell)
             return (1);
         }
         path = oldpwd_env;
-        ft_putstr_fd(path, 1);
-        ft_putstr_fd("\n", 1);
     }
     else
         path = args[1];
@@ -49,12 +55,21 @@ int cd_env_func(char **args, t_shell *shell)
         shell->exit_status = 1;
         return (1);
     }
-    if (!getcwd(old_pwd, sizeof(old_pwd)))
-        return (perror("getcwd"), shell->exit_status = 1, 1);
-    if (update_env_value(&shell->env, "OLDPWD", old_pwd))
-        return (shell->exit_status = 1, 1);
+    if (!getcwd(curr_pwd, sizeof(curr_pwd)))
+    {
+        perror("getcwd");
+        shell->exit_status = 1;
+        return(1);
+    }
+    if (old_pwd_valid || old_pwd[0] == '\0')
+    {
+        if (update_env_value(&shell->env, "OLDPWD", old_pwd))
+            return (shell->exit_status = 1, 1);
+    }
     if (update_env_value(&shell->env, "PWD", curr_pwd))
         return (shell->exit_status = 1, 1);
+    if (args[1] && ft_strncmp(args[1], "-" , 2) == 0)
+        ft_putendl_fd(curr_pwd, STDOUT_FILENO);
     shell->exit_status = 0;
     return (0);
 }
